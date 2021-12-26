6c76ba042619528760ae92a460eb5a69744cdeff
emircan saglam <vsaglam@hotmail.com>
2021-12-26 10:50:38 +0300

    Initial Commit

---
+++ kodlaLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kodlaLayout
+//
+//  Created by Emircan saglam on 26.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
